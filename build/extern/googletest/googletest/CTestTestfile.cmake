08f7d72e5918459019b40f079af99fd379154ecf
Matthew Laidlaw <matthew.james.laidlaw@gmail.com>
2024-07-18 17:03:00 -0700

    add gitignore

--- build/extern/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/matth.MATT/source/repos/Glyph/extern/googletest/googletest
-# Build directory: C:/Users/matth.MATT/source/repos/Glyph/build/extern/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
