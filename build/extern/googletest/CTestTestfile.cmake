db82a729e07e112712c532e5e90ab2c211ff7bf0
Matthew Laidlaw <matthew.james.laidlaw@gmail.com>
2024-07-18 17:01:43 -0700

    visual studio to cmake conversion

---
+++ build/extern/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/matth.MATT/source/repos/Glyph/extern/googletest
+# Build directory: C:/Users/matth.MATT/source/repos/Glyph/build/extern/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ build/extern/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/matth.MATT/source/repos/Glyph/extern/googletest/googlemock
+# Build directory: C:/Users/matth.MATT/source/repos/Glyph/build/extern/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
